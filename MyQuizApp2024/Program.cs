ba31a5c4bc5def55447f0a126c3a3a37ce8022ef
Zcoprich <43861053+Zcoprich@users.noreply.github.com>
2024-07-30 20:51:12 -0500

    Completed MyQuizApp

--- MyQuizApp2024/Program.cs
+++ MyQuizApp2024/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Questions[] questions = new Questions[]
+            {
+                new Questions("What is the capital of Germany?",
+                    new string [] {"Paris", "Berlin", "London", "Madrid"},
+                    1
+                ),
+
+                new Questions("What is 2+2?",
+                    new string [] {"3", "4", "5", "6"},
+                    1
+                ),
+
+                new Questions("What game character cannot swim?",
+                    new string [] {"Mario", "Rayman", "Sonic", "Kirby"},
+                    2
+                )
+            };
+
+            Quiz myQuiz = new Quiz(questions);
+            myQuiz.StartQuiz();
+            Console.ReadLine();
         }
     }
 }
---
+++ MyQuizApp2024/Quiz.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQuizApp2024
+{
+    internal class Quiz
+    {
+        private Questions[] questions;
+        private int score = 0;
+
+        public Quiz(Questions[] questions)
+        {
+           this.questions = questions;
+        }
+
+        public void StartQuiz()
+        {
+            Console.WriteLine("Welcome to the Quiz!");
+            int questionNumber = 1;
+
+            foreach(Questions question in questions)
+            {
+                Console.WriteLine("Question " + questionNumber++ + ":");
+                DisplayQuestion(question);
+                int userChoice = GetUserChoice();
+
+                if (question.isCorrectAnswer(userChoice))
+                {
+                    Console.WriteLine("Correct!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect! The correct answer was: " + question.Answers[question.CorrectAnswerIndex]);
+                }
+            }
+
+            DisplayResult();
+        }
+
+        private void DisplayQuestion(Questions question)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("================================================================");
+            Console.WriteLine("                           Question                             ");
+            Console.WriteLine("================================================================");
+            Console.ResetColor();
+            Console.WriteLine(question.QuestionText);
+
+            for(int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("   ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine(". " + question.Answers[i]);
+            }
+        }
+
+        private int GetUserChoice()
+        {
+            Console.Write("Your answer (number): ");
+            string input = Console.ReadLine();
+            int choice = 0;
+            while (!int.TryParse(input, out choice) || choice < 1 || choice > 4) 
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4: ");
+                input = Console.ReadLine();
+            }
+
+            return choice - 1;
+        }
+        private void DisplayResult()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("================================================================");
+            Console.WriteLine("                            Results                             ");
+            Console.WriteLine("================================================================");
+            Console.ResetColor();
+
+            Console.WriteLine("You have finished the quiz. Your score is: " + score + " out of " + questions.Length);
+
+            double percentage = (double)score / questions.Length;
+            if(percentage == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Amazing! You got a perfect score!");
+            }
+            else if(percentage >= .8)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Excellent! Try again for a better score!");
+            }
+            else if (percentage >= .5)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Good job. Try again for a better score!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Better luck next time. Try again for a better score!");
+            }
+            Console.ResetColor();
+        }
+    }
+}
